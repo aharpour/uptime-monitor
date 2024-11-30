49871b86bedaba052ea56896ea2fea6dbad8a684
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2024-11-30 10:56:54 +0100

    Initial commit!
    (cherry picked from commit 5e57fa9fbcaf781f4c51eb8f5e2d814b7f7117bb)

---
+++ src/main/java/com/aharpour/uptimemonitor/UptimeMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.aharpour.uptimemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptimeMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aharpour/uptimemonitor/UptimeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aharpour.uptimemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
